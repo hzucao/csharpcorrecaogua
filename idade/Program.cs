595f72625390ce4435566ec20c6ca7f1db330231
Heitor Ilario Zucao <heitor.zucao@sp.senai.br>
2023-08-07 15:53:25 -0300

    atividade 01

---
+++ idade/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public static void Main(){
+        Console.WriteLine("Digite sua idade");
+        int idade = int.Parse(Console.ReadLine());
+        int idade10 = idade+10;
+
+        if(idade10 <12){
+            Console.WriteLine("Criança");
+        }
+        else if(idade10 <18){
+            Console.WriteLine("Adolecente");
+        }
+        else if (idade10 <60){
+            Console.WriteLine("Adulto");
+        }
+        else{
+            Console.WriteLine("Idoso");
+        }
+    }
+}
