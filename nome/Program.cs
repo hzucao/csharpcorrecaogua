bc4b404181fc707ae58d9be27e0df6314f311ffb
Heitor Ilario Zucao <heitor.zucao@sp.senai.br>
2023-08-15 12:12:24 -0300

    Aula dia 15/08/2023

---
+++ nome/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+class Progrm{
+    public static void Main(){
+        string[] nomes = new string[10];
+        for(int i =0; i < 10; i++)
+        {
+            Console.WriteLine("Digite um nome:");
+            nomes[i] = Console.ReadLine();
+        }
+
+        Array.Sort(nomes);
+
+        foreach(string n in nomes){
+            Console.WriteLine(n);
+        }
+    }
+}
---
+++ nota/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program{
+    public static void Main(){
+        string[] nomeAluno = new string[3];
+        float[][] nota = new float[3][];
+
+        for(int i = 0; i < 3; i++){
+            Console.WriteLine("Digite o nome do aluno:");
+            nomeAluno[i] = Console.ReadLine();
+            nota[i] = new float[2];
+            Console.WriteLine("Digite a nota 1:");
+            nota[i][0] =  float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a nota 2:");
+            nota[i][1] =  float.Parse(Console.ReadLine());
+        }
+
+         for(int i = 0; i < 3; i++){
+
+            Console.WriteLine("Aluno: " + nomeAluno[i]+" média: "+ (nota[i][0]+nota[i][1])/2)
+         }
+    }
+}
